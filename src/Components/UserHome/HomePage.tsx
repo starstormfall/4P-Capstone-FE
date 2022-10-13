49a4b6db866b6a68e383cf7b199f01379fad9d33
starstormfall <photon_dazzles_02@icloud.com>
2022-10-13 23:10:16 +0800

    added filters for homepage

--- src/Components/UserHome/HomePage.tsx
+++ src/Components/UserHome/HomePage.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { backendUrl } from "../../utils";
 
 // import style components from mantine
-import { Button, Group, Grid, Loader } from "@mantine/core";
+import { Button, Group, Grid, Loader, ScrollArea } from "@mantine/core";
 
 // import interface
 import { Area, Category, Hashtag, Post, PostCard } from "./HomePageInterface";
@@ -17,9 +17,11 @@ export default function HomePage() {
   const [allAreas, setAllAreas] = useState<Area[]>([]);
   const [allCategories, setAllCategories] = useState<Category[]>([]);
   const [allHashtags, setAllHashtags] = useState<Hashtag[]>([]);
-  const [selectedAreas, setSelectedAreas] = useState<number[]>([]);
+  const [selectedAreas, setSelectedAreas] = useState<number | null>();
   const [selectedCategories, setSelectedCategories] = useState<number[]>([]);
   const [selectedHashtags, setSelectedHashtags] = useState<number[]>([]);
+  const [displayCategories, setDisplayCategories] = useState<boolean>(false);
+  const [displayHashtags, setDisplayHashtags] = useState<boolean>(false);
 
   // useEffect api call to get subset of explore posts (need to set up pagination on backend)
   const getExplorePosts = async () => {
@@ -56,22 +58,104 @@ export default function HomePage() {
   useEffect(() => {
     getExplorePosts();
     getAreas();
-    getCategories();
-    getHashtags();
+    // getCategories();
+    // getHashtags();
   }, []);
 
   // handleClickArea ==> api call to get filtered posts based on selected area with all categories and hashtags
-  const handleFilterArea = async (event: MouseEvent) => {
-    const areaId = Number(event.currentTarget.id);
-    const addAreaId: number[] = [...selectedAreas, areaId];
+  const handleFilter = async (event: MouseEvent<HTMLButtonElement>) => {
+    const { id, name } = event.currentTarget;
 
-    setSelectedAreas(addAreaId);
-    console.log(selectedAreas);
+    switch (name) {
+      // if prefecture button is clicked
+      case "prefecture":
+        const areaId = Number(id);
+        // if another area is selected, get filtered posts
+        if (areaId !== selectedAreas) {
+          const areaPosts = await axios.get(
+            `${backendUrl}/posts/explore?areaId=${areaId}`
+          );
+          setAllPosts(areaPosts.data);
+          setSelectedAreas(areaId);
+          setDisplayCategories(true);
+          if (Object.keys(allCategories).length === 0) {
+            getCategories();
+          }
+        } else {
+          // if same area is selected
+          setSelectedAreas(null); // erase selected area
+          setDisplayCategories(false); // erase categories
 
-    const areaPosts = await axios.get(
-      `${backendUrl}/posts/area=${selectedAreas}`
-    );
-    console.log(areaPosts);
+          getExplorePosts(); // reload all posts
+        }
+        setSelectedCategories([]);
+        setDisplayHashtags(false);
+        break;
+      // if category button is clicked
+      case "category":
+        const categoryId = Number(id);
+        let updatedCategoryIds: number[] = [];
+
+        if (selectedCategories.includes(categoryId)) {
+          // remove categoryId if selected before
+          updatedCategoryIds = selectedCategories.filter(
+            (id) => id !== categoryId
+          );
+        } else {
+          // add categoryId to selected
+          updatedCategoryIds = [...selectedCategories, categoryId];
+        }
+
+        if (updatedCategoryIds.length) {
+          // get associated hashtags if 1 or more categories are selected
+          const assocHashtags = await axios.get(
+            `${backendUrl}/info/categories/hashtags?categoryIds=${updatedCategoryIds}`
+          );
+          setAllHashtags(assocHashtags.data);
+          setDisplayHashtags(true);
+          console.log("did this run?");
+          // get all relevant posts filtered by area and category
+          const areaCategoryPosts = await axios.get(
+            `${backendUrl}/posts/explore?areaId=${selectedAreas}&categoryIds=${updatedCategoryIds}`
+          );
+          setAllPosts(areaCategoryPosts.data);
+        } else {
+          // turn off showing hashtags if no categories are selected
+          setDisplayHashtags(false);
+          // get all area posts not filtered by category
+          const areaPosts = await axios.get(
+            `${backendUrl}/posts/explore?areaId=${selectedAreas}`
+          );
+          setAllPosts(areaPosts.data);
+        }
+
+        // const areaCategoriesPost = await axios.get(`${backendUrl}/`);
+
+        setSelectedCategories(updatedCategoryIds);
+        break;
+
+      case "hashtag":
+        console.log("hashtag", name, id);
+        const hashtagId = Number(id);
+        let updatedHashtagIds: number[] = [];
+
+        if (selectedHashtags.includes(hashtagId)) {
+          // remove hashtagId if selected before
+          updatedHashtagIds = selectedHashtags.filter((id) => id !== hashtagId);
+        } else {
+          // add hashtagId to selected
+          updatedHashtagIds = [...selectedHashtags, hashtagId];
+        }
+
+        const areaCategoryHashtagPosts = await axios.get(
+          `${backendUrl}/posts/explore?areaId=${selectedAreas}&categoryIds=${selectedCategories}&hashtagIds=${updatedHashtagIds}`
+        );
+
+        setAllPosts(areaCategoryHashtagPosts.data);
+        setSelectedHashtags(updatedHashtagIds);
+
+        break;
+    }
   };
 
   // display all areas
@@ -80,7 +164,8 @@ export default function HomePage() {
       color="aqua"
       key={index}
       id={`${area.id}`}
-      onClick={handleFilterArea}
+      name="prefecture"
+      onClick={handleFilter}
     >
       {area.prefecture}
     </Button>
@@ -88,13 +173,25 @@ export default function HomePage() {
 
   // display all categories
   const listCategories = allCategories.map((category: Category, index) => (
-    <Button color="blue" key={index}>
+    <Button
+      color="blue"
+      key={index}
+      id={`${category.id}`}
+      name="category"
+      onClick={handleFilter}
+    >
       {category.name}
     </Button>
   ));
   // display all hashtags
   const listHashtags = allHashtags.map((hashtag: Hashtag, index) => (
-    <Button color="purple" key={index}>
+    <Button
+      color="purple"
+      key={index}
+      id={`${hashtag.id}`}
+      name="hashtag"
+      onClick={handleFilter}
+    >
       {hashtag.name}
     </Button>
   ));
@@ -126,17 +223,21 @@ export default function HomePage() {
 
   return (
     <div>
-      <Group>{allAreas && allAreas.length ? listAreas : <Loader />}</Group>
+      <ScrollArea style={{ height: 50 }}>
+        <div style={{ width: "100vw" }}>
+          <Group>{allAreas && allAreas.length ? listAreas : <Loader />}</Group>
+        </div>
+      </ScrollArea>
       <br />
-      <Group>
-        {allCategories && allCategories.length
-          ? listCategories
-          : "Loading Data"}
-      </Group>
+
+      {allCategories && allCategories.length && displayCategories ? (
+        <Group>{listCategories}</Group>
+      ) : null}
+
       <br />
-      <Group>
-        {allHashtags && allHashtags.length ? listHashtags : <Loader />}
-      </Group>
+      {allHashtags && allHashtags.length && displayHashtags ? (
+        <Group>{listHashtags} </Group>
+      ) : null}
       <br />
       <Grid>{allPosts && allPosts.length ? listPosts : <Loader />}</Grid>
     </div>
