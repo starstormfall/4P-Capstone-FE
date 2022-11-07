6b4212855cb8bfaf272d793f366f845412af6134
WorkOutWorkIn <mingshengho96@gmail.com>
2022-11-07 17:22:22 +0800

    threadsingle edit

--- src/Components/TheadSingle.tsx
+++ src/Components/TheadSingle.tsx
@@ -865,7 +865,7 @@ export default function ThreadSingle() {
                             alt={newThreadData[1].post.locationName}
                             height={300}
                             radius="md"
-                            placeholder={<Text align="center">TDFL</Text>}
+                            placeholder={<Text align="center">git TDFL</Text>}
                           />
                           <Text size="md" color="dimmed" lineClamp={4} mt="md">
                             {newThreadData[1].post.content}
